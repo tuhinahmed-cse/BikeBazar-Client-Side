bdec97045aadbf5b4eceace735f6bb1d36b0fbb7
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-24 01:14:21 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="">
+    <div className="max-w-[1440px] mx-auto">
       
       <RouterProvider router={router}></RouterProvider>
       <Toaster></Toaster>
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const menuItems = <>
+
+        <li><Link to="/" className='text-white font-bold'>HOME</Link></li>
+        <li><Link to="/" className='text-white font-bold'>BLOG</Link></li>
+        <li><Link to="/" className='text-white font-bold'>DASHBOARD</Link></li>
+        <li><Link to="/"><button type="button" class="btn btn-active btn-primary">
+            Login</button></Link></li>
+        <li><Link to="/"><button type="button" class="btn btn-active btn-primary">
+            Logout</button></Link></li>
+
+
+
+    </>
+
     return (
-        <div>
-            <h1>Hello nav</h1>
-        </div>
+        <>
+
+            <div className="navbar bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex justify-between">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItems}
+                        </ul>
+                    </div>
+                    <Link to="/" className="btn btn-ghost normal-case text-xl text-white">Bike Bazar</Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        {menuItems}
+                    </ul>
+                </div>
+
+            </div>
+
+        </>
     );
 };
 
