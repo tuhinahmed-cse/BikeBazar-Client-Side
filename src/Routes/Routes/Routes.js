1c50bf19970b3b289ca45a0ad206cd187f2bc80d
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-24 13:00:54 +0600

    firebase and loading add

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
+import Loading from "../../Pages/Shared/Loading/Loading";
 import SignUp from "../../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
             {
                 path: '/register',
                 element: <SignUp></SignUp>
+            },
+            {
+                path: '/load',
+                element: <Loading></Loading>
             }
         ]
     }
